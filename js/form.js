743fbc49d189d7dcb5d518ec17405cef14cc3e5e
Andrii-Zaimak <andreyzaymak@gmail.com>
2017-02-13 12:38:13 +0200

    Добавлены модули initialize-pins и synchronize-fields

--- js/form.js
+++ js/form.js
@@ -3,14 +3,6 @@
  */
 
 'use strict';
-var KEY_CODE_ENTER = 13;
-// var KEY_CODE_ESCAPE = 27;
-var KEY_CODE_SPACE = 32;
-
-var pinsMapNode = document.querySelector('.tokyo__pin-map');
-var pinsListNode = pinsMapNode.querySelectorAll('.pin');
-var dialogNode = document.querySelector('.dialog');
-var dialogCloseBtnNode = dialogNode.querySelector('.dialog__close');
 
 var noticeFormNode = document.querySelector('.notice__form');
 var formTitleNode = noticeFormNode.querySelector('#title');
@@ -22,8 +14,8 @@ var formTypeNode = noticeFormNode.querySelector('#type');
 var formRoomNumberNode = noticeFormNode.querySelector('#room_number');
 var formCapacityNode = noticeFormNode.querySelector('#capacity');
 
-var i;
-var currentPin = null;
+// init pin's
+window.initializePins();
 
 // add validation option to form title
 formTitleNode.required = true;
@@ -39,111 +31,11 @@ formPriceNode.max = 1000000;
 // add validation option to form address
 formAddressNode.required = true;
 
-// add role and tabindex to pin's
-for (i = 0; i < pinsListNode.length; i++) {
-  var element = pinsListNode[i];
-  element.setAttribute('role', 'checkbox');
-  element.setAttribute('tabindex', '0');
+window.synchronizeFields(formTimeNode, formTimeoutNode, ['12', '13', '14'], ['12', '13', '14'], 'value');
 
-  // find active pin
-  if (element.classList.contains('pin--active')) {
-    currentPin = element;
-    element.setAttribute('aria-checked', 'true');
-  } else {
-    element.setAttribute('aria-checked', 'false');
-  }
-}
-
-// add click pin listener
-pinsMapNode.addEventListener('click', function (evt) {
-  setActivePin(evt.target);
-});
-
-pinsMapNode.addEventListener('keydown', function (evt) {
-  if (isValidKeyPressed(evt, [KEY_CODE_ENTER, KEY_CODE_SPACE])) {
-    setActivePin(evt.target);
-  }
-});
-
-// add listener to dialogNode close button
-dialogCloseBtnNode.addEventListener('click', function () {
-  dialogNode.classList.add('dialog--hidden');
-  removePinActivity();
-});
-
-// add change listener to form time and timeout
-formTimeNode.addEventListener('change', function () {
-  formTimeoutNode.value = formTimeNode.value;
-});
-
-formTimeoutNode.addEventListener('change', function () {
-  formTimeNode.value = formTimeoutNode.value;
-});
+window.synchronizeFields(formRoomNumberNode, formCapacityNode, ['1', '2', '100'], ['0', '3', '3'], 'value');
 
 // add change listener to form type
 formTypeNode.addEventListener('change', function () {
   formPriceNode.min = formTypeNode.value;
 });
-
-// add change listener to form room number
-formRoomNumberNode.addEventListener('change', function () {
-  formCapacityNode.value = [2, 100].indexOf(+formRoomNumberNode.value) !== -1 ? 3 : 0;
-});
-
-/**
- * Remove activity from last active pin.
- */
-function removePinActivity() {
-  if (currentPin) {
-    currentPin.setAttribute('aria-checked', 'false');
-    currentPin.classList.remove('pin--active');
-  }
-}
-
-/**
- * Set active pin.
- * @param {Object} target - DOM pin element.
- */
-function setActivePin(target) {
-  target = getPinElement(target);
-
-  // if not found 'div.pin' return
-  if (!target) {
-    return;
-  }
-
-  // remove all activity
-  removePinActivity();
-  // add .pin--active to select element
-  currentPin = target;
-  currentPin.setAttribute('aria-checked', 'true');
-  currentPin.classList.add('pin--active');
-  // show dialogNode
-  dialogNode.classList.remove('dialog--hidden');
-}
-
-/**
- * Check for need key is pressed.
- * @param {Object} evt - keypress object.
- * @param {Array} keyCodes - list of valid keys.
- * @return {boolean} true - if key is valid, false - if keyCode is undefined or invalid.
- */
-function isValidKeyPressed(evt, keyCodes) {
-  return evt.keyCode && keyCodes.indexOf(evt.keyCode) !== -1;
-}
-
-/**
- * Get pin element.
- * @param {Object} target - clicked target.
- * @return {Object|null} null - if target not a 'div.pin'.
- */
-function getPinElement(target) {
-  while (target !== pinsMapNode) {
-    if (target.tagName === 'DIV' && target.classList.contains('pin')) {
-      return target;
-    }
-    target = target.parentNode;
-  }
-
-  return null;
-}
---
+++ js/synchronize-fields.js
@@ -0,0 +1,30 @@
+/**
+ * Created by andre on 13.02.2017.
+ */
+
+'use strict';
+/**
+ * Synchronize two elements.
+ * @param {Object} firstElement - first element.
+ * @param {Object} secondElement - second element.
+ * @param {Array} firstArr - values list of first element.
+ * @param {Array} secondArr - values list of second element.
+ * @param {string} propertyName - changing property of elements.
+ */
+window.synchronizeFields = function (firstElement, secondElement, firstArr, secondArr, propertyName) {
+  var changeElement = function (evt) {
+    var target = evt.target;
+    var index;
+
+    if (target === firstElement) {
+      index = firstArr.indexOf(target[propertyName]);
+      secondElement[propertyName] = secondArr[index];
+    } else {
+      index = secondArr.indexOf(target.value);
+      firstElement[propertyName] = firstArr[index];
+    }
+  };
+
+  firstElement.addEventListener('change', changeElement);
+  secondElement.addEventListener('change', changeElement);
+};
